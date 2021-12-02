35284ce553ec217f6f74abd4e35c2a9b0c7dd7a1
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-12-02 21:14:40 +0200

    refactoring useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,16 +5,23 @@ const useFetch = (fetchFunc, defaultValue, funcParams) => {
     const [state, updateState] = useState(defaultValue);
 
     useEffect(() => {
+        try {
 
         fetchFunc(funcParams)
             .then(data => {
                 updateState(data);
-            })
-            .catch(error => {
-                updateState(error);
             });
+        } catch(error) {
+            console.log(error);
+        }
 
-    }, [fetchFunc, funcParams]);
+
+            // .catch(error => {
+            //     console.log(error);
+            //     updateState(defaultValue);
+            // });
+
+    }, [fetchFunc, funcParams, defaultValue]);
 
     return { state };
 };
